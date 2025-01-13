f0b762fffb6ad506c6e734f686a5381169b919ad
akcocc <akowalski2@cocc.edu>
2025-01-13 09:29:32 -0800

    todo: figure out compression stuff; finished first part of week 2 assgm.

--- week2/KowalskiAlexWeekTwo.py
+++ week2/KowalskiAlexWeekTwo.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
-    print("Hi")
+    x = 57
+    print(x, bin(x), hex(x))
+    x = 57.756
+
+    # x must be an integer
+    # try catch here is a good use case since we are expecting an error to
+    # happen and we want to be able to continue execution as normal anyways
+    try:
+        print(x, bin(x), hex(x))
+    except Exception as err:
+        print(f"parameters must be integers for binary and hex forms; Error message: {err}")
+
+    y = 0b01111111
+    z = 0x80
+    print("Alternitve representation assignment:", y, z)
+
+    print("Sum between representations:", y + z)
